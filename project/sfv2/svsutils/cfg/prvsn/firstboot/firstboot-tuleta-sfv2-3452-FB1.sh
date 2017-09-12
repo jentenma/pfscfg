efab812685dfe95992965963ea65c3a636850eb6
James Entenmann <jentenma@us.ibm.com>
2017-09-12 12:38:42 -0400

    Added svsutils to sfv2 & svrnfs projects

---
+++ project/sfv2/svsutils/cfg/prvsn/firstboot/firstboot-tuleta-sfv2-3452-FB1.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+#
+# Install latest Mellanox
+#
+echo "Updating Mellanox Driver" >> /var/log/firstboot
+#/bin/cd /root/mlnx
+/bin/tar xvf  /root/mlnx/mlnx-en-4.1-1.0.2.0-rhel7.3-ppc64le.tar -C /root/mlnx >> /var/log/firstboot 2>&1
+/root/mlnx/mlnx-en-4.1-1.0.2.0-rhel7.3-ppc64le/install --force >> /var/log/firstboot 2>&1
+/etc/init.d/mlnx-en.d restart >> /var/log/firstboot 2>&1
+mlogs=`ls /tmp/mlnx*`
+echo "Mellanox logs are in $mlogs" >> /var/log/firstboot
+
+echo "Configuring Fabric interface" >> /var/log/firstboot
+cat <<EOF > /etc/sysconfig/network-scripts/ifcfg-fab0
+TYPE="Ethernet"
+BOOTPROTO="static"
+DEFROUTE="no"
+PEERDNS="yes"
+PEERROUTE="yes"
+IPV4_FAILURE_FATAL="no"
+IPV6INIT="yes"
+IPV6_AUTOCONF="yes"
+IPV6_DEFROUTE="yes"
+IPV6_PEERDNS="yes"
+IPV6_PEERROUTES="yes"
+IPV6_FAILURE_FATAL="no"
+NAME="fab0"
+DEVICE="fab0"
+IPADDR=INSERT_FAB_ADDRESS_HERE
+NETMASK="255.255.254.0"
+EOF
+/sbin/ifup fab0
+echo "Done Configuring Fabric interface" >> /var/log/firstboot
+
+#
+# Always at the end
+#
+echo "Cleaning up" >> /var/log/firstboot
+/bin/cat /etc/crontab | /bin/grep -v firstboot > /etc/crontab.tmp
+/bin/cp /etc/crontab /tmp/crontab.fb
+/bin/rm -f /etc/crontab
+/bin/mv /etc/crontab.tmp /etc/crontab
+/bin/cp $0 /tmp/$0.fb
+rm -f $0
